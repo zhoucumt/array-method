24c632f7db8b7982fbb32f84a43d6a8be6891dea
zhoucumt <553817478@qq.com>
2016-09-06 19:09:58 +0800

    shuffle

---
+++ shuffle.js
@@ -0,0 +1,39 @@
+/**
+ * 将数组乱序，复杂度为O(n)
+ * 
+ * @param {Array} arr 需要乱序的数组
+ * @return {Array} res   返回的乱序后的数组
+ */
+function shuffle(arr) {
+
+    /**
+     * 返回一个 [min, max] 范围内的任意整数
+     * 
+     * @param {number} min 范围的最小值
+     * @param {number} max 范围的最大值
+     * @return {number} result   该范围的任意一个整数
+     */
+    function random(min, max) {
+        if (max == null) {
+            max = min;
+            min = 0;
+        }
+        var result = min + Math.floor(Math.random() * (max - min + 1));
+        return result;
+    }
+
+    var length = arr.length;
+    // 结果数组
+    var res = [];
+
+    for (var i = 0, rand; i < length; i++) {
+        // 将当前所枚举位置的元素和 `i=rand` 位置的元素交换
+        rand = random(0, i);
+        if (rand !== i) {
+            res[i] = res[rand];
+        }
+        res[rand] = arr[i];
+    }
+
+    return res;
+}
